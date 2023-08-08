a47c02700b19d7eb860f2c0701a0d426816604aa
Rajeev Tapadia <rtapadia730@gmail.com>
2023-08-09 01:05:12 +0530

    Build Huff Tree

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <queue>
 
 /*
     Todo:
@@ -16,14 +17,24 @@ class node{
     int freq;
     node* left;
     node* right;
+
+    node(char c, int f, node* l, node* r){
+        this->ch = c;
+        this->freq = f;
+        this->left = l;
+        this->right = r;
+    }
 };
 
 // function to find char freq using map
 map<char, int> charFreq(string str){
+    // populating freqTable map
     map<char, int> freqTable;
     for(char i : str)
         freqTable[i]++;
 
+    // printing the freqTable map
+    // this code should be remove afterwards
     map<char, int>:: iterator it = freqTable.begin();;
     while (it != freqTable.end()){
         cout << "Key: " << it->first << " Value: " << it->second << std::endl;
@@ -33,10 +44,46 @@ map<char, int> charFreq(string str){
     return freqTable;
 }
 
-int main(){
-    // char para[] = "linus benedict torvalds is a finnish software engineer who is the creator and historically the lead developer of the linux kernel used by linux distributions and other operating systems such as android he also created the distributed version control system git";
-    string para = string("linus benedict torvalds is a finnish software engineer who is the creator and, historically, the lead developer of the linux kernel, used by linux distributions and other operating systems such as android. he also created the distributed version control system git");
+// object to compare frequency of two nodes
+// used in priority queue
+class comp{
+    public:
+    bool operator()(node* a, node* b){
+        return a->freq > b->freq;
+    }
+};
 
+// function to build huffman tree
+node* BuildHuffTree(map<char, int> freqTable){
+    // creating a priority queue
+    priority_queue<node*, vector<node*>, comp> pq;
+    // wtf is auto??
+    for(auto pair: freqTable){
+        pq.push(new node(pair.first, pair.second, NULL, NULL));
+    }
+
+    // creating the nodes
+    while(pq.size() != 1){
+        node* l = pq.top();
+        pq.pop();
+        node* r = pq.top();
+        pq.pop();
+
+        int sum = l->freq + r->freq;
+        pq.push(new node('\0', sum, l, r));
+    }
+
+    node* root = pq.top();
+    return root;
+}
+
+int main(){
+    // string para = string("linus benedict torvalds is a finnish software engineer who is the creator and, historically, the lead developer of the linux kernel, used by linux distributions and other operating systems such as android. he also created the distributed version control system git");
+
+    string para = string("aaaaabbbcc");
     map<char, int> freqTable = charFreq(para);
 
+    node* huffRoot = BuildHuffTree(freqTable);
+
+    // a bug in tree that needs to be solved
 }
